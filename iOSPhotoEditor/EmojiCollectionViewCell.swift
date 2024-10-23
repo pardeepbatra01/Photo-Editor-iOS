e4a5e5df2315d13541614132a89cb398a2562370
Pardeep <pardeep.batra01@gmail.com>
2024-10-23 16:29:43 +0530

    Editor Complete

---
+++ iOSPhotoEditor/EmojiCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  EmojiCollectionViewCell.swift
+//  Photo Editor
+//
+//  Created by WD on 4/30/17.
+//  Copyright © 2017 WD. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
